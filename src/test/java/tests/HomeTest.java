6254e191933bfc9e0e2ee916bad8caf9198d0cc7
tariq boujja <tariqboujja@tariqs-MBP.lan>
2022-10-12 21:39:46 -0400

    testname tariq

--- src/test/java/tests/HomeTest.java
+++ src/test/java/tests/HomeTest.java
@@ -1,7 +1,9 @@
 package tests;
 
 import base.BaseTest;
+import org.openqa.selenium.By;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 import pages.HomePage;
 
 public class HomeTest extends BaseTest {
@@ -12,5 +14,13 @@ public class HomeTest extends BaseTest {
     public void setUp(){
         page = new HomePage(driver);
     }
+    @Test(testName = "Tariq")
+    public void test01(){
+        String expected = "Automation with Selenium";
+        String actual = driver.findElement(By.id("title")).getText();
+
+        page.assertEquals(actual, expected);
+    }
+
 
 }
