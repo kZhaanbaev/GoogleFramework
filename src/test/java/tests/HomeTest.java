b68a31379fc18c01bb41b5ddf4fd480e90d5fabd
mydle19 <19mydle@gmail.com>
2022-10-12 21:36:09 -0400

    added print line

--- src/test/java/tests/HomeTest.java
+++ src/test/java/tests/HomeTest.java
@@ -2,7 +2,9 @@ package tests;
 
 import base.BaseTest;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 import pages.HomePage;
+import sun.jvm.hotspot.ui.AnnotatedMemoryPanel;
 
 public class HomeTest extends BaseTest {
 
@@ -12,5 +14,8 @@ public class HomeTest extends BaseTest {
     public void setUp(){
         page = new HomePage(driver);
     }
-
+    @Test(testName = "github test")
+    public void testUS001My(){
+        System.out.println("Practicing git");
+    }
 }
