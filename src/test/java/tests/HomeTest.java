eb49761a817752939929aeb08d2d1bb06d0a29f3
AianaSchool <dabanova_anna@mail.ru>
2022-10-12 21:40:20 -0400

    changed file

--- src/test/java/tests/HomeTest.java
+++ src/test/java/tests/HomeTest.java
@@ -2,6 +2,7 @@ package tests;
 
 import base.BaseTest;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 import pages.HomePage;
 
 public class HomeTest extends BaseTest {
@@ -12,5 +13,8 @@ public class HomeTest extends BaseTest {
     public void setUp(){
         page = new HomePage(driver);
     }
-
+       @Test
+    public void aiana() {
+           System.out.println("Hi");
+       }
 }
